d00227af9ddb5b4f5d82efc731e38c6c2a805206
MiticoPlayer <aperper.4@gmail.com>
2022-06-29 22:44:34 +0200

    feat: Api call and fetch data

--- app.js
+++ app.js
@@ -1,39 +1,81 @@
-window.addEventListener('load' , ()=> {
+window.addEventListener("load", () => {
+  let lat;
+  let lon;
 
-    let lat
-    let lon
+  let valorTemp = document.getElementById("valorTemp");
+  let descTemp = document.getElementById("descTemp");
 
-    let valorTemp = document.getElementById('valorTemp')
-    let descTemp = document.getElementById('descTemp')
+  let ubicacion = document.getElementById("ubicacion");
+  let animado = document.getElementById("animado");
 
-    let ubicacion = document.getElementById('ubicacion')
-    let icono = document.getElementById('iconoAnimado')
+  let velocViento = document.getElementById("velocViento");
 
-    let velocViento = document.getElementById('velocViento')
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((posicion) => {
+      lat = posicion.coords.latitude;
+      lon = posicion.coords.longitude;
 
+      //ubicacion actual
+      //const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=3152b6ba37672decf7a09709e96e29c2`;
 
-    if(navigator.geolocation){
-        
-        navigator.geolocation.getCurrentPosition(posicion => {
-            
-            lat = posicion.coords.latitude
-            lon = posicion.coords.longitude
-            
-            const url =  `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={3152b6ba37672decf7a09709e96e29c2}`
+      //ubicacion por ciudad
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=3152b6ba37672decf7a09709e96e29c2`;
+      //   console.log(url);
 
-            console.log(url)
-
-            fetch(url)
-                .then( response => {return response.json})
-                .then( data => {
-                    console.log(data) 
-                    
-                })
-                .catch( error => {
-                    console.log(error) 
-                })
+      fetch(url)
+        .then((response) => {
+          return response.json();
         })
-    }
+        .then((data) => {
+          // console.log(data);
+          //console.log(data.weather[0].main);
+          // console.log(data.name);
 
+          temperatura = Math.round(data.main.temp);
+          valorTemp.textContent = temperatura;
+          descTemp.textContent = data.weather[0].description;
+          ubicacion.textContent = data.name;
+          velocViento.textContent = data.wind.speed;
+          // let iconName = data.weather[0].icon;
+          // const url = `imgs/amcharts_weather_icons_1.0.0/animated/${iconName}`;
 
-})
+          switch (data.weather[0].main) {
+            case "Clouds":
+              animado.src =
+                "imgs/amcharts_weather_icons_1.0.0/animated/cloudy-day-3.svg";
+              break;
+            case "Clear":
+              animado.src =
+                "imgs/amcharts_weather_icons_1.0.0/animated/day.svg";
+              break;
+            case "Thunderstorm":
+              animado.src =
+                "imgs/amcharts_weather_icons_1.0.0/animated/thunder.svg";
+              break;
+            case "Drizzle":
+              animado.src =
+                "imgs/amcharts_weather_icons_1.0.0/animated/rainy-1.svg";
+              break;
+            case "Rain":
+              animado.src =
+                "imgs/amcharts_weather_icons_1.0.0/animated/rainy-3.svg";
+              break;
+            case "Snow":
+              animado.src =
+                "imgs/amcharts_weather_icons_1.0.0/animated/snowy-2.svg";
+              break;
+            case "Atmosphere":
+              animado.src =
+                "imgs/amcharts_weather_icons_1.0.0/animated/cloudy-day-3.svg";
+              break;
+
+            default:
+              break;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
+  }
+});
